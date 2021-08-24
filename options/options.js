daaf0a6a463cc9b9f7c8e915c241e45a0de026b2
Crowdbotics <team@crowdbotics.com>
2021-08-24 15:25:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "bartelsgunja_gamepl_30072Identifier",
+  url: "https://bartelsgunja_gamepl_30072Identifier.botics.co",
+  api: "https://bartelsgunja_gamepl_30072Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
